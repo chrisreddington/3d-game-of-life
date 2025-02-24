6ff21642b641ffc3228e8c0d35c5e76b67911ae1
Chris Reddington <791642+chrisreddington@users.noreply.github.com>
2025-02-24 14:28:15 +0000

    Update base URL in Vite configuration for 3D Game of Life

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/3d-gol/',
+  base: '/3d-game-of-life/',
   plugins: [
     vue(),
   ],
